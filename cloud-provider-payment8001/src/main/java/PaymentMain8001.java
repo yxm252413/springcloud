da1827f8d565f2508536ab7f4361a7968f301dd2
yxm252413 <1174198674@qq.com>
2020-12-12 16:19:44 +0800

    1.新建模块。 2.改模块的pom文件。添加依赖。 3.改yml文件配置。 4.写主启动类。 5.父工程添加依赖和全局版本控制。

---
+++ cloud-provider-payment8001/src/main/java/PaymentMain8001.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname PaymentMain8001
+ * @Description TODO
+ * @Date 2020/12/12
+ * @Created by yxm
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
