e687a355e910a4a2b9c0b3a3dcd92174f15caf39
yxm252413 <1174198674@qq.com>
2020-12-14 21:27:24 +0800

    eureka集群配置： 1.新建7002模块。 2.在pom中添加和7001一样的依赖。 3.修改系统文件： 1）找到C:\Windows\System32\drivers\etc路径下的hosts文件 2）找到文件--选中--右键--属性--把只读取消勾选，打开文件，做修改。 3）在文件的下面添加如下配置： 127.0.0.1  eureka7001.com 127.0.0.1  eureka7002.com 4.修改7001的配置文件，注册7002的服务。 5.添加7002的配置文件，注册7001的服务。 6.添加7002的主启动类。添加@EnableEurekaServer，是eureka服务。 7.测试 1）分别在地址栏输入localhost:7001和localhost:7002.在eureka服务中，如果能看到对应的注册，说明没有问题。 2）分别在地址栏输入http://eureka7001.com:7001/和http://eureka7002.com:7002/在eureka服务中，如果能看到对应的注册，说明没有问题。

---
+++ cloud-eureka-server7002/src/main/java/com/yxm/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.yxm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Classname EurekaMain7002
+ * @Description TODO
+ * @Date 2020/12/14
+ * @Created by yxm
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
