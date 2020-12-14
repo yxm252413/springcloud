111ef088c8285721fdb4ed26555d722cd11d8746
yxm252413 <1174198674@qq.com>
2020-12-14 16:45:51 +0800

    单机版eureka环境搭建： 1.创建模块。 2.添加eureka等相关依赖。 3.配置yml文件。 4.主启动类，添加@EnableEurekaServer.

---
+++ cloud-eureka-server7001/src/main/java/com/yxm/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.yxm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Classname EurekaMain7001
+ * @Description TODO
+ * @Date 2020/12/14
+ * @Created by yxm
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
