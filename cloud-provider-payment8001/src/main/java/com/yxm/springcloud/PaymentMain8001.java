34c8079ec8d40b142c4e54a4948d935b1d16b891
yxm252413 <1174198674@qq.com>
2020-12-14 17:37:01 +0800

    8001注册到eureka： 1.8001pom文件添加eureka客户端依赖。 2.8001配置文件添加eureka配置： eureka:   client:   #  是否将自己注册到eurekaServer，默认为true     register-with-eureka: true     #    是否从eurekaServer服务中抓取注册信息。默认为true，单节点无所谓，集群必须设置为true,才能配合ribbon使用负载均衡     fetchRegistry: true     service-url:       defaultZone: http://localhost:7001/eureka 3.8001主启动类添加注解@EnableEurekaClient 4.启动eureka服务，启动8001服务。查看eureka管控台是否有服务入住，如果有说明测试成功。

--- cloud-provider-payment8001/src/main/java/com/yxm/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/yxm/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.yxm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Classname PaymentMain8001
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Created by yxm
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
