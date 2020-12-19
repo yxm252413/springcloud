eb7e70e35ef5e8f69668854232b2c7a8bc04be76
yxm252413 <1174198674@qq.com>
2020-12-19 23:07:11 +0800

    resttemplate API方法调用： postForEntity getForEntity

--- cloud-consumer-order80/src/main/java/com/yxm/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/yxm/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.yxm.springcloud.controller;
 import com.yxm.springcloud.entities.CommonResult;
 import com.yxm.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +36,21 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/createEntity")
+    public CommonResult<Payment> createEntity(Payment payment) {
+        return restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class).getBody();  //写操作
+    }
+
+    @GetMapping("/consumer/payment/getEntity/{id}")
+    public CommonResult<Payment> getPaymentEntity(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (forEntity.getStatusCode().is2xxSuccessful()) {
+            return forEntity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败", null);
+        }
+
     }
 }
