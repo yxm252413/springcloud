ec23ed5b57564d0786a4e4fea147a75d39edee89
yxm252413 <1174198674@qq.com>
2020-12-21 23:27:10 +0800

    hystrix服务熔断： 1.添加服务熔断接口： 使用hutool包生成uuid:String uuid = IdUtil.simpleUUID(); 2.添加服务熔断注解： //服务熔断     @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback", commandProperties = {             @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),//是否开启断路器             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),//请求次数             @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),//时间窗口期             @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")//失败率达到多少后跳闸     })     //配置参数参考HystrixCommandProperties 3.添加服务熔断兜底方法。 4.测试： 1）启动7001，启动8001hystrix服务。 2）地址栏测试：http://localhost:8001/payment/circuit/1 可以正常访问。 3）地址栏测试： http://localhost:8001/payment/circuit/-1多次点击，使失败率达到60%以上，此时会触发熔断，断路器跳闸。 4）地址栏测试：http://localhost:8001/payment/circuit/1 点击服务还是进入到兜底放，继续刷新地址栏，看到服务慢慢的恢复了正常。称之为链路恢复。 总结：服务降级--服务熔断--恢复调用链路。

--- cloud-provider-hystrix-payment8001/src/main/java/com/yxm/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/yxm/springcloud/controller/PaymentController.java
@@ -38,4 +38,13 @@ public class PaymentController {
         log.info("********************" + result);
         return result;
     }
+
+    //模拟服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("*******result:" + result);
+        return result;
+    }
+
 }
