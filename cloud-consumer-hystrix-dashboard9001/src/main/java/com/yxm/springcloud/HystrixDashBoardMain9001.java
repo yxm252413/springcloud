3f45e706fdab14961904d6a374f99f044eced985
yxm252413 <1174198674@qq.com>
2020-12-22 01:26:44 +0800

    hystrix dashboard图形监控实践： 1.主启动类添加配置： @Bean public ServletRegistrationBean getServlet(){     HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();     ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);     registrationBean.setLoadOnStartup(1);     registrationBean.addUrlMappings("/hystrix.stream");     registrationBean.setName("HystrixMetricsStreamServlet");     return registrationBean; } 如果不加会报错：Unable to connect to Command Metric Stream和404 注意：新版本Hystrix需要在主启动类MainAppHystrix8001中指定监控路径

--- cloud-consumer-hystrix-dashboard9001/src/main/java/com/yxm/springcloud/HystrixDashBoardMain9001.java
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/yxm/springcloud/HystrixDashBoardMain9001.java
@@ -1,8 +1,11 @@
 package com.yxm.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Classname HystrixDashBoardMain9001
@@ -15,5 +18,15 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 public class HystrixDashBoardMain9001 {
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashBoardMain9001.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
