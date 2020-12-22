aec8f83729535bc490c31e70c9d6298744572a68
yxm252413 <1174198674@qq.com>
2020-12-22 18:47:49 +0800

    10个人分4个苹果，每个人至少分一个苹果，10个苹果要分完，问：有多少种分法

--- cloud-consumer-order80/src/test/java/Test.java
+++ cloud-consumer-order80/src/test/java/Test.java
@@ -1,3 +1,8 @@
+import org.assertj.core.util.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Classname Test
  * @Description TODO
@@ -5,8 +10,28 @@
  * @Created by yxm
  */
 public class Test {
+    static List<Integer> result = Lists.newArrayList(0, 0, 0);
+
     public static void main(String[] args) {
         //2147483647是Integer最大值
         System.out.println(Integer.MAX_VALUE);
+//        test();
+    }
+
+    public static void test() {
+        int count = 0;
+        for (int x = 1; x < 9; x++) {
+            for (int y = 1; y < 9; y++) {
+                for (int z = 1; z < 9; z++) {
+                    for (int zz = 1; zz < 9; zz++) {
+                        if (x + y + z + zz == 10) {
+                            System.out.println(x + "-->" + y + "-->" + z + "-->" + z);
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
