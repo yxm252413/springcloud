b6842d71099808753e57d04f8b2d2fc118afbcb1
yxm252413 <1174198674@qq.com>
2020-12-22 19:03:46 +0800

    gateway9527服务搭建： 1.添加一个新模块。 2.修改Pom文件，添加依赖： <!--新增gateway-->         <dependency>             <groupId>org.springframework.cloud</groupId>             <artifactId>spring-cloud-starter-gateway</artifactId>         </dependency> 3.添加yml文件，添加配置 gateway配置：  cloud:     gateway:       routes:         - id: payment_routh #路由的ID，没有固定规则但要求唯一，建议配合服务名           uri: http://localhost:8001   #匹配后提供服务的路由地址           predicates:             - Path=/payment/get/**   #断言,路径相匹配的进行路由
            - id: payment_routh2
              uri: http://localhost:8001
              predicates:
                - Path=/payment/lb/**   #断言,路径相匹配的进行路由
    4.添加主启动类。
    5.测试：
    1）启动7001服务。
    2）启动cloud-provider-payment8001服务。
    3）启动gateway9527服务。
    4）看eureka服务上面是否注册了8001和9527服务：http://localhost:7001/
    访问：http://localhost:8001/payment/get/1
    访问：http://localhost:9527/payment/get/1

---
+++ cloud-gateway-gateway9527/src/main/java/com/yxm/springcloud/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.yxm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Classname GateWayMain9527
+ * @Description TODO
+ * @Date 2020/12/22
+ * @Created by yxm
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
