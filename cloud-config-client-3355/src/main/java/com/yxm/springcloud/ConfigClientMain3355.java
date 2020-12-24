2ddb95f209ee1631e0a4695b759145972bc4af8e
yxm252413 <1174198674@qq.com>
2020-12-24 16:18:07 +0800

    config3355客户端配置和测试： 1.添加3355模块。 2.修改pom文件。 3.添加yml文件，命名为bootstap. 因为bootstap是系统级别的，application是用户级别的，bootstap优先级高，先读取到configserver配置的内容，合并。 4.添加主启动类。 5.添加业务类。 6.测试： 1）启动7001，3344，3355. 2）http://config-3344.com:3344/master/config-dev.yml 2）测试http://localhost:3355/configInfo

---
+++ cloud-config-client-3355/src/main/java/com/yxm/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.yxm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Classname ConfigClientMain3355
+ * @Description TODO
+ * @Date 2020/12/24
+ * @Created by yxm
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/yxm/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.yxm.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname ConfigClientController
+ * @Description TODO
+ * @Date 2020/12/24
+ * @Created by yxm
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
