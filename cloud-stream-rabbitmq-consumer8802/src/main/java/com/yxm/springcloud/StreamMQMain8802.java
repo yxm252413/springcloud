f2e9b4d20555be1a7e237bb0486b9b9e96a6aecb
yxm252413 <1174198674@qq.com>
2020-12-25 00:42:35 +0800

    stream消息驱动之消费者： 1.新建8802模块。 2.pom添加依赖。 <dependency>             <groupId>org.springframework.cloud</groupId>             <artifactId>spring-cloud-starter-stream-rabbit</artifactId>         </dependency> 3.添加yml配置   input: # 这个名字是一个通道的名称           destination: studyExchange # 表示要使用的Exchange名称定义 exchange名字要一致，消费者使用的是input,生产者使用的是output. 4.添加主启动类。 5.添加controller： 1）添加注解： @Component @EnableBinding(Sink.class) //EnableBinding和Sink都是stream包下的。 2）从配置文件中读取数据： @Value("${server.port}") 注解不要写错，${}. 3)方法： //Message是org.springframework.messaging.Message;包下的。 4）添加注解@StreamListener(Sink.INPUT) //StreamListener是org.springframework.cloud.stream.annotation.StreamListener包下的。 6.测试： 1）启动7001，rabbitmq，8801，8802. 2）多次点击：http://localhost:8801/sendMessage 3）观察8801和8802打印次数是否一致，打印信息是否一致。 4）查看rabbit变化： http://localhost:15672/#/exchanges
    踩坑记录：
     1.   @Value("{server.port}")取值错误，因为注解没写对，应该@Value("${server.port}")
     2.  @StreamListener(Sink.INPUT)忘了加注解，会报错，8802无法接受到8801发送的消息

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/yxm/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.yxm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname StreamMQMain8802
+ * @Description TODO
+ * @Date 2020/12/25
+ * @Created by yxm
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
