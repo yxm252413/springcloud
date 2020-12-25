1becdbb1650486590c12987ea32ca7f66e138a2e
yxm252413 <1174198674@qq.com>
2020-12-25 16:27:20 +0800

    stream消息重复消费： 1.新建模块8803 2.添加pom依赖 3.添加yml配置 4.添加主启动类 5.添加业务类 6.测试： 1）启动7001,rabbit,8801，8802，8803 2）8801发送消息http://localhost:8801/sendMessage 3）控制台观察到8802，8803收到相同的消息，从而产生重复消费的问题。 重复消费的问题是：每一个服务消费者默认是一个分组，每一个分组都要接受消息，从而产生重复消费。 解决问题：就是让8802，8803放在相同的组，从而争夺消息。

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/yxm/springcloud/StreamMQMain8803.java
@@ -0,0 +1,19 @@
+package com.yxm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Classname StreamMQMain8803
+ * @Description TODO
+ * @Date 2020/12/25
+ * @Created by yxm
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
