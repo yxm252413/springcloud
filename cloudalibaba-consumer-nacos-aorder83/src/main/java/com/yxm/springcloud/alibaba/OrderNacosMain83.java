7e8482004d355e53471020a1c065d6d8a178b954
yxm252413 <1174198674@qq.com>
2020-12-27 23:44:31 +0800

    nacos服务消费者注册和负载均衡： 1.新建nacos80服务模块。 2.添加依赖。 3.yml配置添加。 service-url: #nacos服务地址http://注册进nacos的服务名   nacos-user-service: http://nacos-payment-provider 4.主启动类添加。 @EnableDiscoveryClient服务注册功能开启。 5.添加restTemplate配置类，@LoadBalanced实现负载均衡和 @Bean交给spring容器管理。 6.添加业务类。 7.测试： 1）找到nacos安装地址运行bin目录下面的cmd，启动nacos服务。 2）启动9001和9002、nacos83 3）地址栏测试 http://localhost:9001/payment/nacos/1 http://localhost:9002/payment/nacos/1 http://localhost:83/consumer/payment/nacos/13 4）查看nacos客户端，服务是否注册进去 http://localhost:8848/nacos/#/serviceManagement?dataId=&group=&appName=&namespace=&serverId= //可以看到2个服务提供者，一个服务消费者。

---
+++ cloudalibaba-consumer-nacos-aorder83/src/main/java/com/yxm/springcloud/alibaba/OrderNacosMain83.java
@@ -0,0 +1,20 @@
+package com.yxm.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Classname OrderNacosMain83
+ * @Description TODO
+ * @Date 2020/12/27
+ * @Created by yxm
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class, args);
+    }
+}
