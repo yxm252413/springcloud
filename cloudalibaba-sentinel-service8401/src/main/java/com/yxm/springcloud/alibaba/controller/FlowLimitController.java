4e7d30ae4d61f09ea811d401117cfcf5acc069b5
yxm252413 <1174198674@qq.com>
2020-12-28 19:18:58 +0800

    sentinel流控规则： 1.快速失败。 2.预热。 3.排队等待。

--- cloudalibaba-sentinel-service8401/src/main/java/com/yxm/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/yxm/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,15 +1,21 @@
 package com.yxm.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Classname FlowLimitController
- * @Description TODO
+ * @Description
+ * 限流：
+ * 1.快速失败源码com.alibaba.csp.sentinel.slots.block.flow.controller.DefaultController
+ * 2.预热源码：com.alibaba.csp.sentinel.slots.block.flow.controller.WarmUpController
+ * 3.排队等候源码：com.alibaba.csp.sentinel.slots.block.flow.controller.RateLimiterController
  * @Date 2020/12/28
  * @Created by yxm
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA() {
@@ -18,7 +24,7 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB() {
-
+        log.info(Thread.currentThread().getName() + "\t" + "testB");
         return "------testB";
     }
 
